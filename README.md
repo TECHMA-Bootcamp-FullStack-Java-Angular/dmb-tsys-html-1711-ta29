322c6c0bbd1dffc804bac99f1188f158790fa3fa
David Maza Borjabad <109163247+d-maza@users.noreply.github.com>
2023-11-18 21:13:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<div align="center"><img width="80%"  src="https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-java-2010-ta15/blob/main/docs/logoDark.png?raw=trueg"  alt="Logo BootCamp" /></div>
+
+# BootCamp T-Systems
+
+![My etiqueta](https://img.shields.io/badge/David%20Maza-DiveCode%F0%9F%90%99-blue) ![UD](https://img.shields.io/badge/TA-29-orange)  ![Java Version](https://img.shields.io/badge/TABLAS-DIV-red)
+
+# T29 – Tablas & Div
+
+Este repositorio contiene las tareas del Bootcamp 👨‍💻
+
+### Rules of use 🚀
+
+- Run the clone repository in your terminal:
+
+``git clone https://github.com/d-maza/dmb-tsys-html-1711-ta29``
+
+
+- Access the folder with the command:
+
+`cd dmb-tsys-html-1711-ta29`
+
+- Open the repository with your IDE
+
+
+[Ver ejercicio ](https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/)  
---
+++ js/lessons.js
@@ -0,0 +1,93 @@
+export const lessons = [
+  {
+    numLessons: 5,
+    issue: 'Tablas & DIV',
+    day: 50, // inicio 27 de Septiembre
+    msg: 'CSS: transformando código en belleza digital.',
+    myProgress: '🥚>🐣>🐤>🐓>🕊️>🦃>🦅>🪂>🛩️>✈️>👨‍🚀>🚀>🛸>🦸‍♂️',
+
+    exercise: [
+      {
+        numExercise: 11,
+        description: `./assets/ej1.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_11',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_11/donde.html',
+     
+
+      },
+      {
+        numExercise: 12,
+        title: '',
+        description: `./assets/ej2.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_12',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_12/instalaciones.html',
+      },
+      {
+        numExercise: 13,
+        title: '',
+        description: `./assets/ej3.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_13',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_13/ejercicio13.html',
+      },
+      {
+        numExercise: 14,
+        title: '',
+        description: `./assets/ej4.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_14',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_14/ejercicio14.html',
+      },     
+      {
+        numExercise: 15,
+        title: '',
+        description: `./assets/ej5.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_15',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_15/ejercicio15.html',
+      },
+      {
+        numExercise: 16,
+        title: '',
+        description: `./assets/ej6.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_16',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_16/ejercicio16.html',
+      },
+      {
+        numExercise: 17,
+        title: '',
+        description: `./assets/ej7.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_17',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_18/ejercicio18.html',
+      },
+      {
+        numExercise: 18,
+        title: '',
+        description: `./assets/ej8.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_18',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29(Ejercicio_18/ejercicio18.html',
+      },
+      {
+        numExercise: 19,
+        title: '',
+        description: `./assets/ej9.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_19',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_19/ejercicio19.html',
+      },
+      {
+        numExercise: 20,
+        title: '',
+        description: `./assets/ej10.png`,
+        urlCode:
+          'https://github.com/TECHMA-Bootcamp-FullStack-Java-Angular/dmb-tsys-html-1711-ta29/tree/main/Ejercicio_20',
+        urlPG: 'https://techma-bootcamp-fullstack-java-angular.github.io/dmb-tsys-html-1711-ta29/Ejercicio_20/ejercicio20.html',
+      },
+    ],
+  },
+]
